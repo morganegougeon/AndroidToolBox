4a87bd378b5a1e7d235d64e26123cff0c3b61851
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-01-21 11:03:19 +0100

    Connexion function & home page

--- app/src/main/java/fr/isen/gougeon/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/gougeon/androidtoolbox/LoginActivity.kt
@@ -1,12 +1,37 @@
 package fr.isen.gougeon.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val goodIdentifier = "admin"
+    val goodPassword = "admin"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        validateButton.setOnClickListener{
+            doLogin()
+        }
+     }
+
+    fun doLogin() {
+        if (canLog(loginInputLayout.text.toString(), passwordInputLayout.text.toString())){
+            val intent = Intent (this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+    fun canLog(identifier : String, password : String) : Boolean
+    {
+        return identifier == goodIdentifier && password == goodPassword
+    }
+
 }
