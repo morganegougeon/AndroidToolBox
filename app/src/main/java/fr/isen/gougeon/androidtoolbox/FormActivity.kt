8024b9308058eec8d753448b44c068ecadd2fd8b
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-01-27 16:45:42 +0100

    Creation of the pop up when data are correctly saved

--- app/src/main/java/fr/isen/gougeon/androidtoolbox/FormActivity.kt
+++ app/src/main/java/fr/isen/gougeon/androidtoolbox/FormActivity.kt
@@ -1,18 +1,15 @@
 package fr.isen.gougeon.androidtoolbox
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
-import android.media.MediaScannerConnection
+import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_form.*
 import org.json.JSONObject
-import java.io.BufferedWriter
 import java.io.File
-import java.io.FileWriter
-import java.io.IOException
 import java.util.*
 
 
@@ -27,7 +24,10 @@ class FormActivity : AppCompatActivity() {
         saveButton.setOnClickListener {
 
             saveData()
+            seeSavedData()
         }
+
+
     }
 
     fun showCalendar() {
@@ -62,13 +62,53 @@ class FormActivity : AppCompatActivity() {
 
     fun saveData()
     {
+        if(nameInputText.text.toString().isNotEmpty() && surnameInputText.text.toString().isNotEmpty() && nameInputText.text.toString().isNotEmpty()) {
+            val file = File(cacheDir.absolutePath + "/data.txt")
+            val json = JSONObject()
+            json.put("name", nameInputText.text)
+            json.put("surname", surnameInputText.text)
+            json.put("birthdate", datePickerDialog.text)
+            file.writeText(json.toString())
+            Toast.makeText(
+                this,
+                "Enregistrement des données reussi " + "${surnameInputText.text}" + "!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        else
+        {
+            Toast.makeText(
+                this,
+                "Veuillez completer tous les champs" ,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+
+
+    }
+
+    fun seeSavedData () {
         val file = File(cacheDir.absolutePath+"/data.txt")
-        val json = JSONObject()
-        json.put("name", nameInputText.text)
-        json.put("surname", surnameInputText.text)
-        json.put("birthdate", datePickerDialog.text)
-        file.writeText(json.toString())
-        Toast.makeText(this, "Enregistrement des données reussi " + "${surnameInputText.text}" + "!", Toast.LENGTH_LONG).show()
+        val readFile = file.readText()
+        val json = JSONObject(readFile)
+
+
+        val dialogBuilder = AlertDialog.Builder(this)
+
+        // set message of alert dialog
+        dialogBuilder.setMessage("Vous etes " + json.get("surname") + " " + json.get("name") + " et vous etes né(e) le " + json.get("birthdate"))
+
+            .setNegativeButton("Fermer", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.cancel()
+            })
+
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Vos données ont bien été sauvegardées !")
+        // show alert dialog
+        alert.show()
 
     }
 
