02cbb89fef256f315209b59bff97acb995a7c8d4
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-01-27 14:23:26 +0100

    Creation of the calendar

--- app/src/main/java/fr/isen/gougeon/androidtoolbox/FormActivity.kt
+++ app/src/main/java/fr/isen/gougeon/androidtoolbox/FormActivity.kt
@@ -1,10 +1,11 @@
 package fr.isen.gougeon.androidtoolbox
 
-import android.content.Context
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_form.*
+import java.util.*
 
 
 class FormActivity : AppCompatActivity() {
@@ -13,8 +14,39 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
-        
+        datePickerDialog.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus){
+                val mYear: Int
+                val mMonth: Int
+                val mDay: Int
+
+                datePickerDialog.clearFocus()
+
+                val c: Calendar = Calendar.getInstance()
+                mYear = c.get(Calendar.YEAR)
+                mMonth = c.get(Calendar.MONTH)
+                mDay = c.get(Calendar.DAY_OF_MONTH)
+
+
+                val datePickerDialog = DatePickerDialog(
+                    this,
+                    OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                        datePickerDialog.setText(
+                            dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year
+                        )
+                    }, mYear, mMonth, mDay
+                )
+                datePickerDialog.show()
+
+            }
+            else {
+
+            }
+        }
+
+
     }
+
 
 
 
